83d2e1c83c061916a798e0ada675208bbd5541bf
David Klingler <deweydbb@gmail.com>
2025-03-08 08:29:57 -0600

    removed gspread for own sheets api

--- function_app.py
+++ function_app.py
@@ -1,9 +1,7 @@
 import azure.functions as func
 import logging
-
+import requests
 import os
-import gspread
-# from gspread_formatting import get_effective_format
 from dateutil.parser import parse
 from datetime import datetime, date, timedelta
 
@@ -12,12 +10,10 @@ from slack_sdk.errors import SlackApiError
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+### START OF CONFIG ###
+
 # do not change (for internal use of the program to map numbers to days)
 DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-
-
-### START OF CONFIG ###
 
 # Can find in the URL: https://docs.google.com/spreadsheets/d/{SHEET_ID}/
 SHEET_ID="1_qWl72k-m8rl9aYLkcvEc1tzHtNEU-Dki0gAdRTIuj0"
@@ -57,10 +53,10 @@ VOLUNTEER_THRESHOLD = 3
 #
 #
 
-slack_token = os.getenv('slack_token')
-client = WebClient(token=slack_token)
-
 def send_message(channel_id, message):
+    slack_token = os.getenv('slack_token')
+    client = WebClient(token=slack_token)
+
     """Send the given message to the specified channel"""
     try:
         # Sending a message to the specified Slack channel
@@ -143,6 +139,138 @@ def send_special_note_message(day_of_week, shift_date, special_notes):
     send_message(channel, message)
 
 
+
+
+
+
+
+
+
+
+
+##########################################################
+
+
+
+
+
+
+## shamelessly stolen from gspread
+def rowcol_to_a1(row: int, col: int) -> str:
+    """Translates a row and column cell address to A1 notation.
+
+    :param row: The row of the cell to be converted.
+        Rows start at index 1.
+    :type row: int, str
+
+    :param col: The column of the cell to be converted.
+        Columns start at index 1.
+    :type row: int, str
+
+    :returns: a string containing the cell's coordinates in A1 notation.
+
+    Example:
+
+    >>> rowcol_to_a1(1, 1)
+    A1
+
+    """
+
+    div = col
+    column_label = ""
+
+    while div:
+        (div, mod) = divmod(div, 26)
+        if mod == 0:
+            mod = 26
+            div -= 1
+        column_label = chr(mod + 64) + column_label
+
+    label = "{}{}".format(column_label, row)
+
+    return label
+
+# returns sheet map that has an index of 0
+def get_default_sheet(sheets):
+    for sheet in sheets:
+        if sheet['properties']['index'] == 0:
+            return sheet
+        
+    return None
+
+# returns the range of all the sheets values in A1 format
+def get_entire_sheet_range(num_rows, num_cols):
+    return f"A1:{rowcol_to_a1(num_rows, num_cols)}"
+
+# returns true if the cells background color rgb channels are all the same
+# false otherwise
+def get_cell_is_gray(cell):
+    effectiveFormat = cell["effectiveFormat"] if "effectiveFormat" in cell else None
+    backgroundColor = effectiveFormat["backgroundColor"] if effectiveFormat is not None and "backgroundColor" in effectiveFormat else None
+    if backgroundColor is not None:
+        # does not always seem to have all three color channels :(
+        red = backgroundColor['red'] if 'red' in backgroundColor else None
+        green = backgroundColor['green'] if 'green' in backgroundColor else None
+        blue = backgroundColor['blue'] if 'blue' in backgroundColor else None
+        if red is not None and green is not None and red != green:
+            return False
+        if red is not None and blue is not None and red != blue:
+            return False
+        if green is not None and blue is not None and green != blue:
+            return False
+
+    return True
+
+# returns true if the cell is a date, false otherwise
+def get_cell_is_date(cell):
+    try:
+        if cell['dataValidation']['condition']['type'] == 'DATE_IS_VALID':
+            return True
+    except Exception:
+        pass # yeah this is lazy but it works
+    return False
+
+
+def get_sheet_data(api_key, sheet_id):
+    base_url = f"https://sheets.googleapis.com/v4/spreadsheets/{sheet_id}"
+    key_param = f"key={api_key}"
+    # what fields to return from the api
+    field_mask = "fields=sheets.properties(index,gridProperties(rowCount,columnCount))"
+    r = requests.get(f"{base_url}?{key_param}&{field_mask}")
+
+    sheets = r.json()['sheets']
+    default_sheet = get_default_sheet(sheets)
+
+    num_rows = default_sheet['properties']['gridProperties']['rowCount']
+    num_cols = default_sheet['properties']['gridProperties']['columnCount']
+    sheet_range = get_entire_sheet_range(num_rows, num_cols)
+
+    # what fields to return from the third api call
+    field_mask = "fields=sheets.data.rowData.values(effectiveFormat.backgroundColor,dataValidation,formattedValue)"
+    r = requests.get(f"{base_url}?{key_param}&ranges={sheet_range}&{field_mask}")
+
+    row_data = r.json()['sheets'][0]['data'][0]['rowData']
+
+    data = []
+
+    for row in row_data:
+        new_row = []
+
+        for cell in row['values']:
+            is_gray = get_cell_is_gray(cell)
+            is_date = get_cell_is_date(cell)
+            value = cell['formattedValue'] if 'formattedValue' in cell else ''
+
+            new_row.append({"is_gray": is_gray, "is_date": is_date, "value": value})
+
+        data.append(new_row)
+
+    return data
+
+
+
+###############################################################
+
 #
 #
 # Start of calender_bot
@@ -150,21 +278,11 @@ def send_special_note_message(day_of_week, shift_date, special_notes):
 #
 
 
-def is_date(string):
-    """Attempt to parse a string into a date object. Otherwise return None"""
-    try:
-        return parse(string).date()
-    except ValueError:
-        return None
-
 def convert_dates(all_cells):
-    """Convert date-like strings in a 2D list to date objects."""
     for row in range(len(all_cells)):
         for col in range(len(all_cells[row])):
-            converted_date = is_date(all_cells[row][col])
-            # check if successfully converted the cell to a date and update it
-            if converted_date:
-                all_cells[row][col] = converted_date
+            if all_cells[row][col]["is_date"]:
+                all_cells[row][col]["value"] = parse(all_cells[row][col]["value"]).date()
 
 def get_date_location(date, all_cells):
     """
@@ -173,26 +291,12 @@ def get_date_location(date, all_cells):
 
     """
     for row_idx, row in enumerate(all_cells):
-        for col_idx, value in enumerate(row):
-            if value == date:
+        for col_idx, cell in enumerate(row):
+            if cell['value'] == date:
                 return row_idx, col_idx  # Return the first occurrence
     raise ValueError(f"Date: {date} not found in the calender")
 
-# def is_special_cell(worksheet, row, col):
-#     """
-#     Determines if a cell is a "special cell" that does not include volunteer info by checking
-#     if the background color is not gray
-#     """
-#     # need to offset 0 based indexing to convert row/column index into excel label. Ex: C15
-#     label = gspread.utils.rowcol_to_a1(row + 1, col + 1)
-#     try:
-#         background_color = get_effective_format(worksheet, label).backgroundColor
-#         return not (background_color.red == background_color.green and background_color.green == background_color.blue)
-#     except AttributeError:
-#         # cell does not have background color, assume is not special
-#         False
-
-def get_voluneers_for_date(date, all_cells, worksheet):
+def get_voluneers_for_date(date, all_cells):
     """
     Returns a tuple of two elements. The first is the list of volunteers for the shift. The second
     Is the list of special cells, like new volunteer that the shift may need to know about
@@ -207,17 +311,17 @@ def get_voluneers_for_date(date, all_cells, worksheet):
     special_rows = []
 
     # go down each row until the next date is reached or the end of the sheet is reached
-    while type(all_cells[row][col]) is str and row < len(all_cells):
+    while type(all_cells[row][col]['value']) is str and row < len(all_cells):
         cell = all_cells[row][col]
 
         # check if the cell is special or contains volunteer signup info
-        if False: # is_special_cell(worksheet, row, col):
-            special_rows.append(cell)
+        if not cell['is_gray']:
+            special_rows.append(cell['value'])
         else:
             # get volunteers names. The cell may contain multiple volunteers sigining up separated
             # by commas so split by commas and then remove any leading/trailing whitespace before
             # adding the volunteers to the volunteers list
-            all_volunteers.extend(item.strip() for item in cell.split(","))
+            all_volunteers.extend(item.strip() for item in cell['value'].split(","))
 
         # go down to the next cell
         row = row + 1
@@ -232,27 +336,14 @@ def get_has_keyholder(volunteers):
     return any(keyholder_mark in volunteer.lower() for keyholder_mark in KEYHOLDER_MARKS for volunteer in volunteers)
 
 def send_slack_messages():
-    # initiate google sheets connection with api key
-    api_key = os.getenv('google_api_key')
-    gc = gspread.api_key(api_key)
+    google_api_key = os.getenv('google_api_key')
 
-    # identify the sheet to work with
-    sh = gc.open_by_key(SHEET_ID)
-
-    # get the first sheet as it appears on google sheets and assume it is the current years calender
-    current_calender = sh.get_worksheet(0)
-
-    # get the entire sheets as a 2D array
-    all_cells = current_calender.get_all_values()
+    # get the entire sheet as a 2D array
+    all_cells = get_sheet_data(google_api_key, SHEET_ID)
 
     # # converts any strings that are dates into date objects
     # # if a cell is not a date, leave as is
     convert_dates(all_cells)
-
-    # volunteers, special_rows = get_voluneers_for_date(date.today() + timedelta(days=6), all_cells, current_calender)
-    # has_keyholder = get_has_keyholder(volunteers)
-
-    # print(volunteers, has_keyholder, special_rows)
 
     today = date.today()
 
@@ -261,21 +352,21 @@ def send_slack_messages():
         day_of_week = DAYS_OF_WEEK[date_to_check.weekday()]
 
         if day_of_week in SHIFT_DAYS:
-            volunteers, _ = get_voluneers_for_date(today + timedelta(days=days_out), all_cells, current_calender)
+            volunteers, _ = get_voluneers_for_date(today + timedelta(days=days_out), all_cells)
             has_keyholder = get_has_keyholder(volunteers)
 
             if len(volunteers) < VOLUNTEER_THRESHOLD or not has_keyholder:
                 send_volunteer_warning_message(day_of_week, date_to_check, volunteers, has_keyholder)
 
-    # for days_out in SHIFT_SPECIAL_NOTES_DAYS:
-    #     date_to_check = today + timedelta(days=days_out)
-    #     day_of_week = DAYS_OF_WEEK[date_to_check.weekday()]
+    for days_out in SHIFT_SPECIAL_NOTES_DAYS:
+        date_to_check = today + timedelta(days=days_out)
+        day_of_week = DAYS_OF_WEEK[date_to_check.weekday()]
 
-    #     if day_of_week in SHIFT_DAYS:
-    #         _, special_cells = get_voluneers_for_date(today + timedelta(days=days_out), all_cells, current_calender)
+        if day_of_week in SHIFT_DAYS:
+            _, special_cells = get_voluneers_for_date(today + timedelta(days=days_out), all_cells)
 
-    #         if special_cells:
-    #             send_special_note_message(day_of_week, date_to_check, special_cells)
+            if special_cells:
+                send_special_note_message(day_of_week, date_to_check, special_cells)
 
 @app.timer_trigger(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
